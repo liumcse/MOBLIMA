8d67828efd49ddb40f5978b45bc7013c8e1d1af9
CL <caoliu98@yahoo.com>
2017-10-28 11:27:22 -0500

    Just Upload
    Need to be edited in the future

--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -1,6 +1,42 @@
 package Model;
 
 public class Seat {
-    private int seatID;
-    private boolean[] booked;
+    private int row;
+    private int col;
+    private boolean booked;
+
+    public Seat(int row, int col) {
+        this.row = row;
+        this.col = col;
+        booked = false;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public String getSeat() {
+        return String.valueOf((char)(65+row)) + (col+1);
+    }
+
+    public void setCol(int colNum) {
+        col = colNum;
+    }
+
+    public void setRow(int rowNum) {
+        row = rowNum;
+    }
+
+    public boolean isBooked() {
+        return booked;
+    }
+
+    public void bookSeat() {
+        booked = true;
+    }
+
 }
